adb30e4f7fbf02b66d4bb330947fb03374a61ad4
aamu9 <aamu9@AMUTHA>
2024-08-21 11:39:19 +0530

    Commit 2

--- C43_Amutha/src/com/tnsif_day1/MyfirstProgram/MyfirstProgram.java
+++ C43_Amutha/src/com/tnsif_day1/MyfirstProgram/MyfirstProgram.java
@@ -3,7 +3,7 @@ package com.tnsif_day1.MyfirstProgram;
 public class MyfirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Welcome to github");
 		// TODO Auto-generated method stub
 
 	}
