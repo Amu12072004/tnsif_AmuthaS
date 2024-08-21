277db088b00a70bb6affc4940300b7015ce727ee
aamu9 <aamu9@AMUTHA>
2024-08-21 12:09:16 +0530

    commit 3

--- C43_Amutha/src/com/tnsif_day5/Constructor/ScannerClass.java
+++ C43_Amutha/src/com/tnsif_day5/Constructor/ScannerClass.java
@@ -6,7 +6,7 @@ public class ScannerClass {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in); // Syntax for the scanner class
-		System.out.println("Enter your name:");
+		System.out.println("Enter your word:");
 		String str = sc.nextLine();
 		System.out.println("Your name is "+str);
 
